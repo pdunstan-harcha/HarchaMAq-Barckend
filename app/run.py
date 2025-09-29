0e239266ca08e68a155705fd90630fa2d35dd34b
pdunstan-harcha <pdunstan@harcha.cl>
2025-09-29 12:41:26 -0300

    feat: actualizar la configuración del puerto en run.py para usar la variable de entorno PORT

--- app/run.py
+++ app/run.py
@@ -6,4 +6,5 @@ app = create_app()
 
 if __name__ == '__main__':
     debug = os.getenv("FLASK_DEBUG", "1") in ("1", "true", "True")
-    app.run(host='0.0.0.0', port=5000, debug=debug)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=debug)
